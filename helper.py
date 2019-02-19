d8c1fa7cf3d90e8cc8c1c36002c92cef4e643bc3
Ali Shokoohi <alishokoohi666@gmail.com>
2019-02-19 17:00:23 +0330

    Fix reading data from database

--- helper.py
+++ helper.py
@@ -8,26 +8,28 @@ def was_here(user_id):
 	c = cnn.cursor()
     #Create database if not exists
 	c.execute('''CREATE TABLE IF NOT EXISTS users (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    user_id TEXT NOT NULL, username TEXT NOT NULL, Date DATETIME NOT NULL)''')
+    user_id TEXT NOT NULL, first_name TEXT NOT NULL, last_name TEXT NOT NULL, username TEXT NOT NULL, Date DATETIME NOT NULL)''')
 	users = c.execute("SELECT * FROM users")
-	users = dict(users)
-	cnn.close()
     #Check is user_id in users informations
 	for user in users:
 		chat_id = user[1]
 		if user_id == chat_id:
 			found = True
 			break
+	cnn.close()
 	return found
 
 #Write new user to database
 def write(user_id, first_name, last_name, user_name):
-	now = str(datetime.now())#Get current time as string
+	now = str(datetime.now())[:-7]#Get current time until second as string
 	cnn = sqlite3.connect("database/users.db")#Connect to database
 	cnn.execute("PRAGMA ENCODING = 'utf8';")#Set encoding uft-8
 	cnn.text_factory = str
 	c = cnn.cursor()
 	c.execute('''CREATE TABLE IF NOT EXISTS users (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, user_id TEXT NOT NULL, username TEXT NOT NULL, Date DATETIME NOT NULL)''')
-	c.execute("INSERT INTO `users`(`user_id`,`username`,`Date`) VALUES ('{0}', '{1}', '{2}');".format(user_id, user_name, now))
+	insert = "INSERT INTO `users`(`user_id`, `first_name`, `last_name`, `username`, `Date`) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}');\
+		".format(user_id, first_name, last_name, user_name, now)
+	print(insert)
+	c.execute(insert)
 	cnn.commit()
 	cnn.close()
