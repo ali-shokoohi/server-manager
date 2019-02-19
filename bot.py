1b2da5f5fac041392dff2edd6b2f7d4719b106b2
Ali Shokoohi <alishokoohi666@gmail.com>
2019-02-19 13:16:00 +0330

    Initial commit!

---
+++ bot.py
@@ -0,0 +1,32 @@
+# -------- Telepot --------------#
+import telepot
+from telepot.loop import MessageLoop
+# ---------- Other --------------#
+from time import sleep
+
+#Set bot token
+TOKEN = '732974516:AAHp_tZ8UHTgP0FBuAfejFIchXYufcio3Yw'
+bot = telepot.Bot(TOKEN)
+
+#Handeling commands
+def handle(msg):
+    content_type = telepot.glance(msg)[0]
+    if content_type == 'text':
+        cmd = msg['text']
+        chat_id = msg['chat']['id']
+        msg_id = msg['message_id']
+        if cmd == "/start":
+            output = "Working ..."
+            bot.sendMessage(chat_id=chat_id, text=output)
+        else:
+            pass #TODO: Handling other commands
+    else:
+        pass #TODO: Handling file messages
+
+
+MessageLoop(bot, handle).run_as_thread()#Handel commands
+
+print("Working ... ")
+
+while True:#Always awake
+    sleep(10)
